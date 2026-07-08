09dbd92efb0d849748ac35f545e05a8a41b60c24
AnekTheGreat <166177289+AnekTheGreat@users.noreply.github.com>
2026-07-07 23:10:52 -0400

    Update CACHE_NAME from v7 to v9

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'sidecut-shell-v7';
+const CACHE_NAME = 'sidecut-shell-v9';
 const SHELL_FILES = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', (event) => {
   event.waitUntil(
