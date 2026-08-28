c882fa3708e2e74097b5c08a00b4d24ae72bc5ba
AnekTheGreat <166177289+AnekTheGreat@users.noreply.github.com>
2026-08-28 18:08:59 +0000

    Bump SW to v51.72

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'sidecut-shell-v51.71';
+const CACHE_NAME = 'sidecut-shell-v51.72';
 const SHELL_FILES = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
